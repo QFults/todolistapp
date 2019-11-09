a385dc9f1a6f225029cb2e9fe79cb1b1cd3711bd
Quinton Fults <quinton.fults.work@gmail.com>
2019-11-08 17:44:03 -0800

    commit 1

---
+++ sql/seed.sql
@@ -0,0 +1,5 @@
+USE todo_db;
+
+INSERT INTO items (text, isDone)
+VALUES ("Take out trash", false),
+("Clean dishes", true), ("Walk the dog", false), ("Pick up dry cleaning", true), ("Cook dinner", false);
